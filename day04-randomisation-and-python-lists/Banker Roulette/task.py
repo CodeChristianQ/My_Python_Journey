1e0a90e631130604a1504e95c03e3749c3637b7c
Christian Quintero <xrpchristianq@gmail.com>
2025-07-19 20:18:36 -0400

    Remove Day 4 project folder from repository to comply with copyright

--- day04-randomisation-and-python-lists/Banker Roulette/task.py
+++
@@ -1,8 +0,0 @@
-#My completed version 07/11/2025
-import random
-friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
-#option 1 to solve
-number_of_friends = random.randint(0,4)
-print(friends[number_of_friends])
-#option 2 to solve
-print(random.choice(friends))
--- day04-randomisation-and-python-lists/Lists/task.py
+++
@@ -1,5 +0,0 @@
-#My completed version 07/11/2025
-usa = ["florida", "new york", "Maryland"]
-print(usa[1])
-usa[1] = "NY"
-print(usa)
