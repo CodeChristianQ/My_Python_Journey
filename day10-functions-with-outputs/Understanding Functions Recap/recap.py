7b356561c1acb851a3770c0f3cda3be91284277a
Christian Quintero <xrpchristianq@gmail.com>
2025-07-27 10:06:03 -0400

    Test adding subfolders for recap and review on lessons.

---
+++ day10-functions-with-outputs/Understanding Functions Recap/recap.py
@@ -0,0 +1,8 @@
+#🔁 Step-by-Step Hands-On Activities 07/27/2025
+# TODO 1. Function Anatomy Drill Create 10 functions with one clear purpose each. Example:
+#def greet(name):
+    # return f"Hello, {name}!"
+# TODO a. Write the function
+# TODO b. Call it with 2–3 different arguments
+# TODO c. Print the result
+
