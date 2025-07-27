cc216b7c2de796eced9d6f95e5defad7e5eab39f
Christian Quintero <xrpchristianq@gmail.com>
2025-07-27 10:31:23 -0400

    Added to the recap file that was previously pushed to Git.

--- day10-functions-with-outputs/Understanding Functions Recap/recap.py
+++ day10-functions-with-outputs/Understanding Functions Recap/recap.py
@@ -6,3 +6,15 @@
 # TODO b. Call it with 2–3 different arguments
 # TODO c. Print the result
 
+def greet_with_name(name):
+    """This function takes the argument ie(Person's name) and sets the parameter ie(name)."""
+    return f"Hello {name}?"
+
+#below are the various arguments stored in variables
+person1 = greet_with_name("Christian")
+person2 = greet_with_name("Kaiju")
+person3 = greet_with_name("Igris")
+#below we print each variable that calls the function with our various arguments.
+print(person1)
+print(person2)
+print(person3)
